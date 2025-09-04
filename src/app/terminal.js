c5dbf251ef8c00935b81f4cf5a853fba310dd7f2
Riley Fischer <rfmineguy@gmail.com>
2025-09-04 16:42:21 -0700

    [Terminal] implement the ls command utilizing the filesystem abstraction

--- src/app/terminal.js
+++ src/app/terminal.js
@@ -4,6 +4,22 @@ class Terminal {
     this.fs = new FileSystem();
     this.cwd = this.fs.getRoot();
     this.commandMap = {
+      'ls': (args) => {
+        var dir = undefined;
+        if (args.length == 1) dir = this.cwd;
+        else if (args[1] === '/') dir = this.fs.getRoot();
+        else if (args[1][0] === '/') dir = this.fs.getRoot().searchPath(args[1].slice(1));
+        else dir = this.fs.getRoot().searchPath(args[1]);
+        if (dir) {
+          for (let i = 0; i < dir.directories.length; i++) console.log(` Directory -  ${dir.directories[i].name}`)
+          for (let i = 0; i < dir.files.length; i++) console.log(` File      -  ${dir.files[i]}`)
+        }
+        else {
+          console.error(`directory ${args[1]} does not exist`);
+        }
+      },
+      'cd': (args) => {
+      }
     };
   }
 
