e262cfa84632e33c72594927ece45bd0f48f278a
Riley Fischer <rfmineguy@gmail.com>
2025-09-04 22:22:34 -0700

    [Command;ls] fix subtle bug where  used root as the start instead of the cwd

--- src/app/commands/ls.js
+++ src/app/commands/ls.js
@@ -9,7 +9,7 @@ class LsCommand extends AbstractCommand {
     if (args.length == 1) dir = this.terminal.cwd;
     else if (args[1] === '/') dir = this.filesystem.getRoot();
     else if (args[1][0] === '/') dir = this.filesystem.getRoot().searchPath(args[1].slice(1));
-    else dir = this.filesystem.getRoot().searchPath(args[1]);
+    else dir = this.terminal.cwd.searchPath(args[1]);
     if (dir) {
       for (let i = 0; i < dir.directories.length; i++) result.putOutputLine(`drwxr-xr-x@ Sep 4 10:00 ${dir.directories[i].name}`)
       for (let i = 0; i < dir.files.length; i++)       result.putOutputLine(`-rw-r--r--@ Sep 4 10:00 ${dir.files[i]}`)
