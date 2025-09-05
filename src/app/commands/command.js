44ecba93ccb8bf70ab29aff1423e7a19d1e57568
Riley Fischer <rfmineguy@gmail.com>
2025-09-04 21:52:09 -0700

    [Command] Create a class for representing the result of a command run

---
+++ src/app/commands/command.js
@@ -0,0 +1,62 @@
+class CommandResult {
+  constructor(dir, args) {
+    this.dir = dir
+    this.args = args;
+    this.raw_command = ''
+    this.output_lines = []
+    this.code = 0
+  }
+
+  putOutputLine(line) {
+    this.output_lines.push(line)
+  }
+
+  createCommandDOM() {
+    console.log(this.args)
+    const div = document.createElement('div')
+    const text = document.createElement('p')
+    text.innerText = `${this.dir.realpath()} $ ${this.args.join(' ')}`
+    div.appendChild(text)
+    return div
+  }
+
+  createSuccessDOM() {
+    const div = document.createElement('div')
+    div.appendChild(this.createCommandDOM())
+    for (let line of this.output_lines) {
+      const el = document.createElement('p')
+      el.innerText = line
+      div.appendChild(el)
+    }
+    return div
+  }
+
+  createFailDOM() {
+    const div = document.createElement('div')
+    div.appendChild(this.createCommandDOM())
+    for (let line of this.output_lines) {
+      const el = document.createElement('p')
+      el.innerText = line
+      div.appendChild(el)
+    }
+    return div
+  }
+
+  createDOM() {
+    if (this.code == 0) return this.createSuccessDOM()
+    else return this.createFailDOM()
+  }
+
+  static NoInput(dir, args) {
+    var result = new CommandResult(dir, args)
+    result.code = 1
+    return result
+  }
+
+  static CommandNotFound(name, dir, args) {
+    var result = new CommandResult(dir, args)
+    result.putOutputLine(`Command ${name} not found`)
+    result.code = 1
+    return result
+  }
+}
