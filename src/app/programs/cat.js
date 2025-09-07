e50e963e431fa18980fb870ee87616e3530a7a70
Riley Fischer <rfmineguy@gmail.com>
2025-09-06 22:08:51 -0700

    [Program; cat] implement a program for printing the contents of a file

---
+++ src/app/programs/cat.js
@@ -0,0 +1,23 @@
+class CatProgram extends Program {
+  constructor(parent_shell) {
+    super(parent_shell)
+  }
+
+  main(args) {
+    var file = undefined
+    if (args.length == 1) {
+      this.print(`cat: requires one argument`)
+      return 1
+    }
+    else if (args[1][0] === '/') file = this.parent_shell.fs.getRoot().searchPath(args[1].slice(1));
+    else file = this.parent_shell.cwd.searchPath(args[1]);
+
+    if (file instanceof Directory) {
+      this.print(`cat: ${args[1]}: not a directory`)
+      return 1
+    }
+
+    this.print(file.contents)
+    return 0
+  }
+}
