8a55146a786a5da4e196a9b51a240ad10f465d5f
Riley Fischer <rfmineguy@gmail.com>
2025-09-06 21:37:17 -0700

    Main: move a lot of this logic into the terminal via the forwardKey method

--- src/app/main.js
+++ src/app/main.js
@@ -1,48 +1,15 @@
-let term = new Terminal();
-let ignoreEvents = ['Shift', 'Meta', 'Alt', 'Control', 'Escape', 'Tab', 'Caps Lock']
-
 const prompt = document.getElementById('prompt')
 const termOutput = document.getElementById('term-output')
 const termArea = document.getElementsByClassName('term-area')[0]
 
-const updateline = (ch) => {
-  const input = document.getElementById('term-input-text')
-  input.innerText = term.input;
-
-  const prompt = document.getElementById('prompt')
-  prompt.innerText = `${term.cwd.realpath()} $`
-};
+let term = new Terminal(prompt, termOutput, termArea);
 
 const handlekey = (event) => {
-  if (ignoreEvents.includes(event.key)) return;
   if (event.key === '/' && !["INPUT", "TEXTAREA"].includes(event.target.tagName))
     event.preventDefault();
 
-  if (event.key === 'Backspace') term.deleteCharFromInput();
-  else if (event.key === 'Enter') {
-    const result = term.submitInput();
-    if (result.output_lines[0] === '<clear>') {
-      termOutput.replaceChildren();
-    }
-    else {
-      const dom = result.createDOM()
-      termOutput.appendChild(dom)
-      termArea.scrollTop = termArea.scrollHeight;
-    }
-  }
-  else if (event.key == 'ArrowUp') {
-    term.navigateHistory(1)
-    event.preventDefault()
-  }
-  else if (event.key == 'ArrowDown') {
-    term.navigateHistory(-1)
-    event.preventDefault()
-  }
-  else term.addCharToInput(event.key);
-
-  updateline(term.input);
+  var preventEvent = term.forwardKey(event.key)
+  if (preventEvent) event.preventDefault()
 };
-
-updateline()
 
 document.addEventListener('keydown', handlekey)
