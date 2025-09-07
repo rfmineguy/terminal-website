b00f3339a005023820d8168f47df9850fb397568
Riley Fischer <rfmineguy@gmail.com>
2025-09-06 21:04:10 -0700

    Terminal: Implement some functions for writing content onto the terminal visually

--- src/app/terminal.js
+++ src/app/terminal.js
@@ -30,7 +30,29 @@ class Terminal {
     return div
   }
 
+  writePrompt(text) {
+    console.log(text)
+    this.prompt_node.innerText = text
+  }
 
+  write(text) {
+    const lines = text.split("\n")
+    if (lines.length == 0) return
+    for (let line of lines) {
+      this.line = ""
+      for (let i = 0; i < line.length; i++) {
+        if (line[i] == '\r') this.line = ""
+        else if (line.slice(i).startsWith('\\033')) {
+          // process escape code
+          console.log('substr', line.slice(i).startsWith('\\033'))
+          this.output_dom_node.replaceChildren();
+          break
+        }
+        else this.line += text[i]
+      }
+      const dom = this.createDomLine(this.line)
+      this.output_dom_node.appendChild(dom)
+      this.term_area_dom_node.scrollTop = this.term_area_dom_node.scrollHeight;
     }
   }
 }
