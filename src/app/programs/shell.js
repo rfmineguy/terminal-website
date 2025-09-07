91550fd1aabc577d7268fc166f4a63673e735773
Riley Fischer <rfmineguy@gmail.com>
2025-09-06 20:07:25 -0700

    [Program; shell] setup constructor of shell

--- src/app/programs/shell.js
+++ src/app/programs/shell.js
@@ -1,5 +1,20 @@
+let ignoreEvents = ['Shift', 'Meta', 'Alt', 'Control', 'Escape', 'Tab', 'CapsLock']
+
 class Shell {
   constructor(parent_terminal) {
+    this.buf = ""
+    this.parent_terminal = parent_terminal
+    this.process_stack = []
+    this.fs = new FileSystem()
+    this.cwd = this.fs.getRoot();
+    this.programs = {
+      'ls': new LsProgram(this),
+      'cd': new CdProgram(this),
+      'clear': new ClearProgram(this)
+    };
+    this.commandHistory = []
+    this.commandHistoryIndex = -1
+    this.write_cmdline()
   }
 
   write_cmdline() {
