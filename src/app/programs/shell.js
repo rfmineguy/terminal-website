10a56a4140009d403baf3d27391037544ef5a83b
Riley Fischer <rfmineguy@gmail.com>
2025-09-06 20:06:28 -0700

    [Program; shell] setup skeleton of shell program

---
+++ src/app/programs/shell.js
@@ -0,0 +1,16 @@
+class Shell {
+  constructor(parent_terminal) {
+  }
+
+  write_cmdline() {
+  }
+
+  terminal_keyevent(key) {
+  }
+
+  submitInput() {
+  }
+
+  navigateHistory(direction) {
+  }
+}
